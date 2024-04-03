257bafe62bb44f517fdd865d976c96a7fee75ef6
matyasgergo <matyasgergo@gmail.com>
2024-04-03 11:31:28 +0200

    updates

--- server/api/descriptor.ts
+++ server/api/descriptor.ts
@@ -1,3 +1,13 @@
+import descriptorTemplate from "../../lib/descriptor/descriptor-template.json";
+
 export default defineEventHandler(async (event) => {
-  return "Hello World";
+  let descriptorTemplateWithoutSchema = JSON.parse(
+    JSON.stringify(descriptorTemplate).replaceAll(
+      "{{APP_HOST}}",
+      event.req.headers.host ?? ""
+    )
+  ) as Omit<typeof descriptorTemplate, "$schema"> & Record<string, unknown>;
+  delete descriptorTemplateWithoutSchema["$schema"];
+
+  return descriptorTemplateWithoutSchema;
 });
